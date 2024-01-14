af60a97d4afdec3892d111e14d2c97f15a2bf09c
Manuel Gil <manuel.gil@ua.pt>
2024-01-14 17:41:05 +0000

    exports

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,13 @@ export default defineConfig({
         // make sure to externalize deps that shouldn't be bundled
         // into your library
         external: ['react','react/jsx-runtime'],
+        output: {
+            // Provide global variables to use in the UMD build
+            // for externalized deps
+            globals: {
+              react: 'React',
+            },
+          },
       },
   }
 })
