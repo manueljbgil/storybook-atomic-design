f77fdd3dd2381cfa68a81bbb73bd954eb36399f9
Manuel Gil <manuel.gil@ua.pt>
2024-01-14 16:14:59 +0000

    added more configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ['lib'] })],
   build:{
+    copyPublicDir:false,
     lib:{
         entry: resolve(__dirname, 'lib/main.ts'),
         formats: ['es'],
@@ -13,7 +15,7 @@ export default defineConfig({
     rollupOptions: {
         // make sure to externalize deps that shouldn't be bundled
         // into your library
-        external: ['react'],
+        external: ['react','react/jsx-runtime'],
       },
   }
 })
